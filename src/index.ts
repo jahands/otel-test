e0709d674022ad41aac1bbed725be7167e897c99
Jacob Hands <jacob@gogit.io>
2023-07-06 20:13:31 -0500

    Add queues

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { trace } from '@opentelemetry/api'
+import { instrument, ResolveConfigFn } from '@microlabs/otel-cf-workers'
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+	HONEYCOMB_API_KEY: string
+	OTELTEST: Queue<any>
 }
 
-export default {
-	async fetch(
-		request: Request,
+const handler = {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const fetchCount = 100
+		const promises: Promise<any>[] = []
+		for (let i = 0; i < fetchCount; i++) {
+			const fn = async () => {
+				// All 100 of these show up in the fetch trace
+				await env.OTELTEST.send({ i })
+			}
+			promises.push(fn())
+		}
+
+		await Promise.all(promises)
+
+		return new Response(`hello world!`)
+	},
+
+	async queue(
+		batch: MessageBatch<any>,
 		env: Env,
 		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
+	) {
+		const uuids: string[] = []
+		for (let i = 0; i < batch.messages.length; i++) {
+			const promises: Promise<any>[] = []
+			const fn = async () => {
+				// Not all of these spans show up in honeycomb :(
+				// Also tried enclosing this in another span, but that didn't help
+				const res = await fetch('https://uuid.rocks/plain')
+				const uuid = await res.text()
+				uuids.push(uuid)
+			}
+
+			promises.push(fn())
+			await Promise.all(promises)
+		}
+		// This prints out 100 uuids
+		console.log(uuids)
+		console.log('uuids.length', uuids.length)
 	},
-};
+}
+
+const config: ResolveConfigFn = (env: Env, _trigger) => {
+	return {
+		exporter: {
+			url: 'https://api.honeycomb.io/v1/traces',
+			headers: { 'x-honeycomb-team': env.HONEYCOMB_API_KEY },
+		},
+		service: { name: 'otel-test', namespace: 'test' },
+		fetch: {
+			includeTraceContext: false,
+		},
+	}
+}
+
+export default instrument(handler, config)
